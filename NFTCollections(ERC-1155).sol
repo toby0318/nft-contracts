cd514097f0afe01aafae27670095563b3bd9ca03
toby0318 <78232000+toby0318@users.noreply.github.com>
2022-01-08 13:10:10 +0900

    add string library

--- NFTCollections(ERC-1155).sol
+++ NFTCollections(ERC-1155).sol
@@ -1069,72 +1069,97 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI, TokenURI {
 
 pragma solidity >=0.6.0 <0.8.0;
 
-  /**
-   * @dev Contract module which provides a basic access control mechanism, where
-   * there is an account (an owner) that can be granted exclusive access to
-   * specific functions.
-   *
-   * By default, the owner account will be the one that deploys the contract. This
-   * can later be changed with {transferOwnership}.
-   *
-   * This module is used through inheritance. It will make available the modifier
-   * `onlyOwner`, which can be applied to your functions to restrict their use to
-   * the owner.
-   */
-  abstract contract Ownable is Context {
-      address private _owner;
+/**
+  * @dev Contract module which provides a basic access control mechanism, where
+  * there is an account (an owner) that can be granted exclusive access to
+  * specific functions.
+  *
+  * By default, the owner account will be the one that deploys the contract. This
+  * can later be changed with {transferOwnership}.
+  *
+  * This module is used through inheritance. It will make available the modifier
+  * `onlyOwner`, which can be applied to your functions to restrict their use to
+  * the owner.
+  */
+abstract contract Ownable is Context {
+    address private _owner;
 
-      event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-      /**
-       * @dev Initializes the contract setting the deployer as the initial owner.
-       */
-      constructor () internal {
-          address msgSender = _msgSender();
-          _owner = msgSender;
-          emit OwnershipTransferred(address(0), msgSender);
-      }
+    /**
+      * @dev Initializes the contract setting the deployer as the initial owner.
+      */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
 
-      /**
-       * @dev Returns the address of the current owner.
-       */
-      function owner() public view returns (address) {
-          return _owner;
-      }
+    /**
+      * @dev Returns the address of the current owner.
+      */
+    function owner() public view returns (address) {
+        return _owner;
+    }
 
-      /**
-       * @dev Throws if called by any account other than the owner.
-       */
-      modifier onlyOwner() {
-          require(_owner == _msgSender(), "Ownable: caller is not the owner");
-          _;
-      }
+    /**
+      * @dev Throws if called by any account other than the owner.
+      */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
 
-      /**
-       * @dev Leaves the contract without owner. It will not be possible to call
-       * `onlyOwner` functions anymore. Can only be called by the current owner.
-       *
-       * NOTE: Renouncing ownership will leave the contract without an owner,
-       * thereby removing any functionality that is only available to the owner.
-       */
-      function renounceOwnership() public virtual onlyOwner {
-          emit OwnershipTransferred(_owner, address(0));
-          _owner = address(0);
-      }
+    /**
+      * @dev Leaves the contract without owner. It will not be possible to call
+      * `onlyOwner` functions anymore. Can only be called by the current owner.
+      *
+      * NOTE: Renouncing ownership will leave the contract without an owner,
+      * thereby removing any functionality that is only available to the owner.
+      */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
 
-      /**
-       * @dev Transfers ownership of the contract to a new account (`newOwner`).
-       * Can only be called by the current owner.
-       */
-      function transferOwnership(address newOwner) public virtual onlyOwner {
-          require(newOwner != address(0), "Ownable: new owner is the zero address");
-          emit OwnershipTransferred(_owner, newOwner);
-          _owner = newOwner;
-      }
-  }
+    /**
+      * @dev Transfers ownership of the contract to a new account (`newOwner`).
+      * Can only be called by the current owner.
+      */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
 
+library Strings {
+
+    function concat(string memory _base, string memory _value) internal pure returns (string memory) {
+        bytes memory _baseBytes = bytes(_base);
+        bytes memory _valueBytes = bytes(_value);
+
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
+        bytes memory _newValue = bytes(_tmpValue);
+
+        uint i;
+        uint j;
+
+        for(i=0; i<_baseBytes.length; i++) {
+            _newValue[j++] = _baseBytes[i];
+        }
+
+        for(i=0; i<_valueBytes.length; i++) {
+            _newValue[j++] = _valueBytes[i];
+        }
+
+        return string(_newValue);
+    }
+
+}
 
 contract Terraworld is ERC1155, Ownable {
+  using Strings for string;
   // Hashes of meme pictures on IPFS
   string[] public hashes;
   // Collection Logo
@@ -1142,7 +1167,7 @@ contract Terraworld is ERC1155, Ownable {
   // CollectionName
   string public name;
   // CollectionSymbol
-  string public symbol = "NFT Token";
+  string public symbol;
   // Mapping for enforcing unique hashes
   mapping(string => bool) _hashExists;
 
@@ -1160,8 +1185,9 @@ contract Terraworld is ERC1155, Ownable {
   event minting(uint256 id, string indexed hash, string indexed uri, uint256 amount);
 
   constructor(string memory _name, string memory _collectionUri) public ERC1155(_collectionUri) {
-      name = _name;
+      name = _name.concat(" NFT");
       logo = _collectionUri;
+      symbol = _name;
   }
 
   function safeTransferFrom(
